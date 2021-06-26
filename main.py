be59e660b544f13b6ddcc75a8088561ebbeb8bcb
olafnekeman <olafnekeman@gmail.com>
2021-06-26 14:21:19 +0200

    changed the main.py to a dcc.store session

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
+import collections
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
 import pandas as pd
-from dash.dependencies import Input, Output
+
+from dash.dependencies import Output, Input
+from dash.exceptions import PreventUpdate
+
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -16,29 +15,76 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    'a': [1, 2, 3],
-    'b': [4, 1, 4],
-    'c': ['x', 'y', 'z'],
-})
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+countries = set(df['country'])
+
 
 app.layout = html.Div([
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in df['c'].unique()],
-        value='a'
-    ),
-    html.Div(id='output'),
+    dcc.Store(id='memory-output'),
+    dcc.Dropdown(id='memory-countries', options=[
+        {'value': x, 'label': x} for x in countries
+    ], multi=True, value=['Canada', 'United States']),
+    dcc.Dropdown(id='memory-field', options=[
+        {'value': 'lifeExp', 'label': 'Life expectancy'},
+        {'value': 'gdpPercap', 'label': 'GDP per capita'},
+    ], value='lifeExp'),
+    html.Div([
+        dcc.Graph(id='memory-graph'),
+        dash_table.DataTable(
+            id='memory-table',
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+    ])
 ])
 
-@app.callback(Output('output', 'children'),
-              Input('dropdown', 'value'))
-def update_output_1(value):
-    # Safely reassign the filter to a new variable
-    filtered_df = df[df['c'] == value]
-    return len(filtered_df)
+
+@app.callback(Output('memory-output', 'data'),
+              Input('memory-countries', 'value'))
+def filter_countries(countries_selected):
+    if not countries_selected:
+        # Return all the rows on initial load/no country selected.
+        return df.to_dict('records')
+
+    filtered = df.query('country in @countries_selected')
+
+    return filtered.to_dict('records')
+
+
+@app.callback(Output('memory-table', 'data'),
+              Input('memory-output', 'data'))
+def on_data_set_table(data):
+    if data is None:
+        raise PreventUpdate
+
+    return data
+
+
+@app.callback(Output('memory-graph', 'figure'),
+              Input('memory-output', 'data'),
+              Input('memory-field', 'value'))
+def on_data_set_graph(data, field):
+    if data is None:
+        raise PreventUpdate
+
+    aggregation = collections.defaultdict(
+        lambda: collections.defaultdict(list)
+    )
+
+    for row in data:
+
+        a = aggregation[row['country']]
+
+        a['name'] = row['country']
+        a['mode'] = 'lines+markers'
+
+        a['x'].append(row[field])
+        a['y'].append(row['year'])
+
+    return {
+        'data': [x for x in aggregation.values()]
+    }
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, threaded=True, port=10450)
