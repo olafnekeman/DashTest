a8eb76e8e650286c5bf09d38f388a6a390ca44a4
olafnekeman <olafnekeman@gmail.com>
2021-06-26 13:40:29 +0200

    Added requirmentss

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [4, 1, 4],
+    'c': ['x', 'y', 'z'],
+})
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in df['c'].unique()],
+        value='a'
+    ),
+    html.Div(id='output'),
+])
+
+@app.callback(Output('output', 'children'),
+              Input('dropdown', 'value'))
+def update_output_1(value):
+    # Safely reassign the filter to a new variable
+    filtered_df = df[df['c'] == value]
+    return len(filtered_df)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
